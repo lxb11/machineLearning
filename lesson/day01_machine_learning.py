581988111be354f2ee15de5092036f1e43cc2101
lvzhao <623417725@qq.com>
2022-08-14 09:57:57 +0800

    特征预处理，特征降维

--- lesson/day01_machine_learning.py
+++ lesson/day01_machine_learning.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import PCA
 import jieba
+from scipy.stats import pearsonr
 
 
 def datasets_demo():
@@ -114,6 +119,107 @@ def count_chinese_demo2():
     return None
 
 
+def tfidf_demo():
+    """
+    用TF-IDF的方法进行文本特征抽取
+    :return:
+    """
+
+    # 1、讲中文文本进行分词
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
+
+    data_new = []
+    for sent in data:
+        data_new.append(cut_word(sent))
+    print(data_new)
+    # 1、实例化一个转换器类
+    transfer = TfidfVectorizer(stop_words=["一种", "所以"])
+
+    # 2、调用fit_transform
+    data_final = transfer.fit_transform(data_new)
+    print("data_final: \n", data_final.toarray())
+    print("特征名字: \n", transfer.get_feature_names())
+
+    return None
+
+
+def minmax_demo():
+    """
+    归一化
+    :return:
+    """
+    # 1、获取数据
+    data = pd.read_csv("datingTestSet2.csv")
+    data = data.iloc[:, :3]
+    print("data: \n", data)
+
+    # 2、实例化一个转换器类
+    transfer = MinMaxScaler(feature_range=(2, 3))
+    # 3、调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new: \n", data_new)
+    return None
+
+
+def stand_demo():
+    """
+    标准化
+    :return:
+    """
+    # 1、获取数据
+    data = pd.read_csv("datingTestSet2.csv")
+    data = data.iloc[:, :3]
+    print("data: \n", data)
+
+    # 2、实例化一个转换器类
+    transfer = StandardScaler()
+    # 3、调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new: \n", data_new)
+    return None
+
+
+def variance_demo():
+    """
+    过滤低方差特征
+    :return:
+    """
+    # 1、获取数据
+    data = pd.read_csv("factor_returns.csv")
+    data = data.iloc[:, 1:-2]
+    print("data: \n", data)
+
+    # 2、实例化一个转换器类
+    transfer = VarianceThreshold(threshold=10)
+    # 3、调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new: \n", data_new, data_new.shape)
+
+    # 计算某两个变量之间的相关系数
+    r1 = pearsonr(data["pe_ratio"], data["pb_ratio"])
+    print("相关系数：\n", r1)
+    r2 = pearsonr(data["revenue"], data["total_expense"])
+    print("相关系数：\n", r2)
+
+    return None
+
+
+def pca_demo():
+    """
+    PCA 降维
+    :return:
+    """
+    data = [[2, 8, 4, 5], [6, 3, 0, 8], [5, 4, 9, 1]]
+    # 1、实例化一个转化器类
+    transfer = PCA(n_components=0.95)
+    # 2、调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new: \n", data_new)
+    return None
+
+
 if __name__ == "__main__":
     # 代码1：sklearn数据集使用
     # datasets_demo()
@@ -124,6 +230,16 @@ if __name__ == "__main__":
     # 代码4：中文文本特征抽取：CountVectorizer
     # count_chinese_demo()
     # 代码5：中文文本特征抽取：自动分词
-    count_chinese_demo2()
+    # count_chinese_demo2()
     # 代码6：中文分词
     # print(cut_word("我爱北京天安门"))
+    # 代码7：用TF-IDF的方法进行文本特征抽取
+    # tfidf_demo()
+    # 代码8：归一化
+    # minmax_demo()
+    # 代码9：标准化
+    # stand_demo()
+    # 代码10：低方差特征过滤
+    # variance_demo()
+    # 代码11：PCA降维
+    pca_demo()
