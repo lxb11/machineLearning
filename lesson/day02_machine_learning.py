c61f1ddc9ff6c502a87b012f275c906aff0a397d
lvzhao <623417725@qq.com>
2022-08-20 21:23:19 +0800

    用决策树对鸢尾花进行分类

--- lesson/day02_machine_learning.py
+++ lesson/day02_machine_learning.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 
 def knn_iris():
@@ -19,7 +20,7 @@ def knn_iris():
     # 1）获取数据
     iris = load_iris()
     # 2）划分数据集
-    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=6)
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=22)
     # 3）特征工程：标准化
     transfer = StandardScaler()
     # x_train = transfer.fit_transform(x_train)
@@ -192,12 +193,40 @@ def nb_news():
     return None
 
 
+def decision_iris():
+    """
+    用决策树对鸢尾花进行分类
+    :return:
+    """
+    # 1）获取数据集
+    iris = load_iris()
+    # 2）划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=22)
+    # 3）决策树预估器
+    estimator = DecisionTreeClassifier(criterion="entropy")
+    estimator.fit(x_train, y_train)
+    # 4）模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值:\n", y_test == y_predict)
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+    # 可视化决策树
+    export_graphviz(estimator, out_file="./iris_tree.dot", feature_names=iris.feature_names)
+    print("done")
+    return None
 
 
 if __name__ == "__main__":
+    # 代码1：用KNN算法对鸢尾花进行分类
+    # knn_iris()
     # 代码2：用KNN算法对鸢尾花进行分类
     # knn_iris_gscv()
     # 代码3：facebook 案例分析
     # knn_facebook_demo()
-    # 代码四：用朴素贝叶斯算法对新闻进行分类
-    nb_news()
+    # 代码4：用朴素贝叶斯算法对新闻进行分类
+    # nb_news()
+    # 代码5：用决策树对鸢尾花进行分类
+    decision_iris()
