c53cf25ecda241e96a542b8468ccfcc1d9f81d35
lvzhao <623417725@qq.com>
2022-08-20 10:01:03 +0800

    facebook案例分析

--- lesson/day02_machine_learning.py
+++ lesson/day02_machine_learning.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 def knn_iris():
@@ -37,6 +39,121 @@ def knn_iris():
     return None
 
 
+def knn_iris_gscv():
+    """
+    用KNN算法对鸢尾花进行分类
+    :return:
+    """
+    # 1）获取数据
+    iris = load_iris()
+    # 2）划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=6)
+    # 3）特征工程：标准化
+    transfer = StandardScaler()
+    # x_train = transfer.fit_transform(x_train)
+    transfer.fit(x_train)
+    x_train = transfer.transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4）KNN算法预估器
+    estimator = KNeighborsClassifier(n_neighbors=3)
+
+    # 加入网格搜索与交叉验证
+    # 参数准备
+    param_dict = {"n_neighbors": [1, 3, 5, 7, 9, 11]}
+    estimator = GridSearchCV(estimator, param_grid=param_dict, cv=10)
+    estimator.fit(x_train, y_train)
+    # 5）模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值:\n", y_test == y_predict)
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 最佳参数：best_params_
+    print("最佳参数：\n", estimator.best_params_)
+    # 最佳结果：best_score_
+    print("最佳结果：\n", estimator.best_score_)
+    # 最佳估计器：best_estimator_
+    print("最佳估计器：\n", estimator.best_estimator_)
+    # 交叉验证结果：cv_results_
+    print("交叉验证结果：\n", estimator.cv_results_)
+
+    return None
+
+
+def knn_facebook_demo():
+    """
+    facebook 案例分析
+    :return:
+    """
+    # 1、获取数据
+    data = pd.read_csv("D:/pyhtonProject/Machine_Learning/resources/FBlocation/train.csv")
+    data.head()
+    # 2、基本数据处理
+    # 1）缩小数据范围
+    data = data.query("x < 2.5 & x > 2 & y < 1.5 & y > 1.0")
+    # 2) 处理时间戳
+    time_value = pd.to_datetime(data["time"], unit="s")
+    date = pd.DatetimeIndex(time_value)
+    # print(time_value)
+    # print(date)
+    data["day"] = date.day
+    data["weekday"] = date.weekday
+    data["hour"] = date.hour
+    # 3) 过滤签到次数少的地点
+    place_count = data.groupby("place_id").count()["row_id"]
+    place_count[place_count > 3].head()
+    data_final = data[data["place_id"].isin(place_count[place_count > 3].index.values)]
+    # 筛选特征值和目标值
+    x = data_final[["x", "y", "accuracy", "day", "weekday", "hour"]]
+    y = data_final["place_id"]
+    # 数据集划分
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=6)
+    # 3）特征工程：标准化
+    transfer = StandardScaler()
+    # x_train = transfer.fit_transform(x_train)
+    transfer.fit(x_train)
+    x_train = transfer.transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4）KNN算法预估器
+    estimator = KNeighborsClassifier(n_neighbors=3)
+
+    # 加入网格搜索与交叉验证
+    # 参数准备
+    param_dict = {"n_neighbors": [3, 5, 7, 9]}
+    estimator = GridSearchCV(estimator, param_grid=param_dict, cv=3)
+    estimator.fit(x_train, y_train)
+    # 5）模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值:\n", y_test == y_predict)
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 最佳参数：best_params_
+    print("最佳参数：\n", estimator.best_params_)
+    # 最佳结果：best_score_
+    print("最佳结果：\n", estimator.best_score_)
+    # 最佳估计器：best_estimator_
+    print("最佳估计器：\n", estimator.best_estimator_)
+    # 交叉验证结果：cv_results_
+    print("交叉验证结果：\n", estimator.cv_results_)
+
+    print("data_final: \n", data_final)
+    print("x.head(): \n", x.head())
+    print("y: \n", y)
+
+    return None
+
+
 if __name__ == "__main__":
-    # 代码1：用KNN算法对鸢尾花进行分类
-    knn_iris()
+    # 代码2：用KNN算法对鸢尾花进行分类
+    # knn_iris_gscv()
+    # 代码3：facebook 案例分析
+    knn_facebook_demo()
