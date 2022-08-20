0b82ba3c3f6fd023cfc003e9abc2dbc186fdc9c2
lvzhao <623417725@qq.com>
2022-08-20 18:06:30 +0800

    用朴素贝叶斯算法对新闻进行分类

--- lesson/day02_machine_learning.py
+++ lesson/day02_machine_learning.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 
 def knn_iris():
@@ -152,8 +155,49 @@ def knn_facebook_demo():
     return None
 
 
+def nb_news():
+    """
+    用朴素贝叶斯算法对新闻进行分类
+    :return:
+    """
+    # 1) 获取数据
+    news = fetch_20newsgroups(data_home="./", subset="all")
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(news.data, news.target)
+    # 3) 特征工程： 文本特征抽取-tfidf
+    transfer = TfidfVectorizer()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+    # 4) 朴素贝叶斯算法预估器流程
+    estimator = MultinomialNB()
+    estimator.fit(x_train, y_train)
+
+    # 5) 模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值:\n", y_test == y_predict)
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # # 最佳参数：best_params_
+    # print("最佳参数：\n", estimator.best_params_)
+    # # 最佳结果：best_score_
+    # print("最佳结果：\n", estimator.best_score_)
+    # # 最佳估计器：best_estimator_
+    # print("最佳估计器：\n", estimator.best_estimator_)
+    # # 交叉验证结果：cv_results_
+    # print("交叉验证结果：\n", estimator.cv_results_)
+    return None
+
+
+
+
 if __name__ == "__main__":
     # 代码2：用KNN算法对鸢尾花进行分类
     # knn_iris_gscv()
     # 代码3：facebook 案例分析
-    knn_facebook_demo()
+    # knn_facebook_demo()
+    # 代码四：用朴素贝叶斯算法对新闻进行分类
+    nb_news()
