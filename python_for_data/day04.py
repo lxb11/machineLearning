6a4032636c243949a63c81573e81e39e9b65fd4a
lxb11 <623417725@qq.com>
2022-11-07 23:28:20 +0800

    ndarray的数据类型

--- python_for_data/day04.py
+++ python_for_data/day04.py
@@ -76,7 +76,62 @@ def demo03():
     return None
 
 
+def demo04():
+    """
+    ndarray的数据类型
+    :return:
+    """
+    # dtype（数据类型）是一个特殊的对象，它含有ndarray将一块内存解释为特定数据类型所需的信息
+    arr1 = np.array([1, 2, 3], dtype=np.float64)
+    arr2 = np.array([1, 2, 3], dtype=np.int32)
+    print(arr1.dtype)
+    print(arr2.dtype)
+    '''
+    dtype是NumPy灵活交互其它系统的源泉之一。
+    多数情况下，它们直接映射到相应的机器表示，
+    这使得“读写磁盘上的二进制数据流”以及“集成低级语言代码
+    （如C、Fortran）”等工作变得更加简单。数值型dtype的命名方式相同：
+    一个类型名（如float或int），后面跟一个用于表示各元素位长的数字。
+    标准的双精度浮点值（即Python中的float对象）需要占用8字节（即64位）。
+    因此，该类型在NumPy中就记作float64。表4-2列出了NumPy所支持的全部数据类型。
+    '''
+    '''
+    笔记：记不住这些NumPy的dtype也没关系，新手更是如此。
+    通常只需要知道你所处理的数据的大致类型是浮点数、复数、整数、布尔值、字符串，
+    还是普通的Python对象即可。当你需要控制数据在内存和磁盘中的存储方式时（尤其是对大数据集），
+    那就得了解如何控制存储类型。
+    '''
+    # 你可以通过ndarray的astype方法明确地将一个数组从一个dtype转换成另一个dtype
+    arr = np.array([1, 2, 3, 4, 5])
+    print(arr.dtype)
+    float_arr = arr.astype(np.float64)
+    print(float_arr.dtype)
+    # 在本例中，整数被转换成了浮点数。如果将浮点数转换成整数，则小数部分将会被截取删除
+    arr3 = np.array([3.7, -1.2, -2.6, 0.5, 12.9, 10.1])
+    print(arr3.dtype)
+    print(arr3.astype(np.int32))
+    # 如果某字符串数组表示的全是数字，也可以用astype将其转换为数值形式
+    numeric_strings = np.array(['1.25', '-9.6', '42'], dtype=np.string_)
+    print(numeric_strings.astype(float))
+    # 注意：使用numpy.string_类型时，一定要小心，
+    # 因为NumPy的字符串数据是大小固定的，发生截取时，
+    # 不会发出警告。pandas提供了更多非数值数据的便利的处理方法。
+    # 如果转换过程因为某种原因而失败了（比如某个不能被转换为float64的字符串），
+    # 就会引发一个ValueError。这里，我比较懒，写的是float而不是np.float64；
+    # NumPy很聪明，它会将Python类型映射到等价的dtype上
+
+    # 数组的dtype还有另一个属性
+    int_array = np.arange(10)
+    calibers = np.array([.22, .270, .357, .380, .44, .50], dtype=np.float64)
+    print(int_array.astype(calibers.dtype))
+    # 你还可以用简洁的类型代码来表示dtype
+    empty_uint32 = np.empty(8, dtype='u4')
+    print(empty_uint32)
+    return None
+
+
 if __name__ == "__main__":
     # demo01()
     # demo02()
-    demo03()
+    # demo03()
+    demo04()
